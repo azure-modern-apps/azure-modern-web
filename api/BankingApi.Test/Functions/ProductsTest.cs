1a0d5a7e2bdc349ef293ef9e1905897cfcd1af9d
Dul Wimalatissa <Dul.Wimalatissa@insight.com>
2021-01-23 10:39:58 +1000

    Implement first pass of products api

---
+++ api/BankingApi.Test/Functions/ProductsTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using BankingApi.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace BankingApi.Test.Functions
+{
+    [TestClass]
+    public class ProductsTest : TestBase
+    {
+        [TestMethod]
+        public async Task Get_ReturnsProducts()
+        {
+            var func = new BankingApi.Functions.Products(CreateDbContext());
+            var response = (OkObjectResult)await func.Run(CreateHttpRequest(), Factory.CreateLogger<BankingApi.Functions.Products>());
+            var responseBody = JsonConvert.DeserializeObject<IList<ProductResponse>>(response.Value.ToString());
+
+            Assert.IsTrue(response.StatusCode == (int)HttpStatusCode.OK);
+            Assert.AreEqual(3, responseBody.Count);
+        }
+    }
+}
---
+++ api/BankingApi.Test/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using BankingApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BankingApi.Test
+{
+    public abstract class TestBase
+    {
+        protected static readonly LoggerFactory Factory = new LoggerFactory();
+
+        public BankingContext CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BankingContext>();
+            optionsBuilder.UseInMemoryDatabase("Banking");
+
+            return new BankingContext(optionsBuilder.Options);
+        }
+
+        public HttpRequest CreateHttpRequest()
+        {
+            var context = new DefaultHttpContext();
+            return context.Request;
+        }
+    }
+}
