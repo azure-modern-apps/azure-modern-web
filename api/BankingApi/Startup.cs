95c7a7e2373c0c1443215ce1301b5323dc422854
Dul Wimalatissa <40200505+dwimalatissa@users.noreply.github.com>
2021-02-01 09:41:30 +1000

    Add key vault support via MSI (#50)
    Co-authored-by: Dul Wimalatissa <Dul.Wimalatissa@insight.com>

--- api/BankingApi/Startup.cs
+++ api/BankingApi/Startup.cs
@@ -1,7 +1,10 @@
-﻿using BankingApi;
+﻿using System;
+using Azure.Identity;
+using BankingApi;
 using BankingApi.Data;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(Startup))]
@@ -12,16 +15,32 @@ namespace BankingApi
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            ConfigureServices(builder.Services);
+            var configuration = builder.GetContext().Configuration;
+
+            ConfigureServices(builder.Services, configuration);
         }
 
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<BankingContext>(options =>
                 options.UseCosmos(
-                    "",
-                    "",
-                    ""));
+                    configuration["CosmosEndpoint"],
+                    configuration["CosmosAccountKey"],
+                    configuration["CosmosDatabaseName"]));
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            var vaultUri = new Uri(Environment.GetEnvironmentVariable("KeyVaultUri") ?? throw new InvalidOperationException());
+            var vaultTenantId = Environment.GetEnvironmentVariable("KeyVaultTenantId");
+
+            var azureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+            {
+                ExcludeSharedTokenCacheCredential = true,
+                VisualStudioTenantId = vaultTenantId
+            });
+
+            builder.ConfigurationBuilder.AddAzureKeyVault(vaultUri, azureCredential);
         }
     }
 }
